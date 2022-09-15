2d19b4f048da7201b7b5b5da905f9e2019c437b5
bloweyelashwish <yelizaveta.barkar@gmail.com>
2022-09-15 22:39:51 +0300

    Handle deletion / test

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blogs');
+const {request} = require("express");
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({})
@@ -15,6 +16,12 @@ blogsRouter.post('/', async (request, response) => {
     }
 
     response.status(201).json(body)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndRemove(request.params.id)
+    console.log(request.params.id)
+    response.status(204).end()
 })
 
 module.exports = blogsRouter;
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -17,99 +17,122 @@ beforeEach(async () => {
 
 const api = supertest(app)
 
-test('blogs are returned as json', async() => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-}, 100000)
+describe('when blogs exist', () => {
+    test('blogs are returned as json', async() => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    }, 100000)
 
-test('there is one blog', async () => {
-    const response = await api.get('/api/blogs')
+    test('there is one blog', async () => {
+        const response = await api.get('/api/blogs')
 
-    expect(response.body).toHaveLength(1)
+        expect(response.body).toHaveLength(1)
+    })
+
+    test('the author of the first blog is Mark', async () => {
+        const response = await api.get('/api/blogs')
+
+        expect(response.body[0].author).toBe('Mark')
+    })
+
+    test('all blogs are returned', async () => {
+        const response = await api.get('/api/blogs')
+
+        expect(response.body).toHaveLength(helper.initialBlogs.length)
+    })
+
+    test('a specific blog is within the returned blogs', async () => {
+        const response = await api.get('/api/blogs')
+        const authors = response.body.map(b => b.author);
+
+        expect(authors).toContain('Aaron')
+    })
 })
 
-test('the author of the first blog is Mark', async () => {
-    const response = await api.get('/api/blogs')
+describe('when POSTing', () => {
+    test('a valid blog can be added', async () => {
+        const newBlog = helper.initialBlogs[0]
 
-    expect(response.body[0].author).toBe('Mark')
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const blogsAtEnd = await helper.blogsInDb()
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+        const authors = blogsAtEnd.map(b => b.author)
+        expect(authors).toContain('Jaron')
+    })
+
+    test('blog has valid data', async () => {
+        const newBlog = {
+            title: "Wont work"
+        }
+
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(400)
+
+        const blogsAtEnd = await helper.blogsInDb()
+
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+    })
+
+    test('existence of id prop', async () => {
+        const response = await api.get('/api/blogs')
+
+        expect(response.body[0].id).toBeDefined()
+    })
+
+    test('likes default to 0', async () => {
+        await api
+            .post('/api/blogs')
+            .send(helper.initialBlogs[0])
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const response = await api.get('/api/blogs')
+
+        expect(response.body[0].likes).toBe(0);
+    })
+
+    test('verify if title and post are missing', async () => {
+        const falsyPost = {
+            author: 'Who knows',
+            likes: 140
+        }
+
+        await api
+            .post('/api/blogs')
+            .send(falsyPost)
+            .expect(400)
+
+        const notesAtEnd = await helper.blogsInDb()
+        console.log(helper.initialBlogs.length === notesAtEnd.length)
+        expect(notesAtEnd).toHaveLength(helper.initialBlogs.length)
+    })
 })
 
-test('all blogs are returned', async () => {
-    const response = await api.get('/api/blogs')
+describe('when modifying existing blogs', () => {
+    test('when deleting', async () => {
+        const blogsAtStart = await helper.blogsInDb()
+        const toDel = blogsAtStart[0]
 
-    expect(response.body).toHaveLength(helper.initialBlogs.length)
-})
+        await api
+            .delete(`/api/blogs/${toDel.id}`)
+            .expect(204)
 
-test('a specific blog is within the returned blogs', async () => {
-    const response = await api.get('/api/blogs')
-    const authors = response.body.map(b => b.author);
+        const blogsAtEnd = await helper.blogsInDb()
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
 
-    expect(authors).toContain('Aaron')
-})
-
-test('a valid blog can be added', async () => {
-    const newBlog = helper.initialBlogs[0]
-
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-
-    const blogsAtEnd = await helper.blogsInDb()
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-
-    const authors = blogsAtEnd.map(b => b.author)
-    expect(authors).toContain('Jaron')
-})
-
-test('blog has valid data', async () => {
-    const newBlog = {
-        title: "Wont work"
-    }
-
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(400)
-
-    const blogsAtEnd = await helper.blogsInDb()
-
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
-})
-
-test('existence of id prop', async () => {
-    const response = await api.get('/api/blogs')
-
-    expect(response.body[0].id).toBeDefined()
-})
-
-test('likes default to 0', async () => {
-    await api
-        .post('/api/blogs')
-        .send(helper.initialBlogs[0])
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-
-    const response = await api.get('/api/blogs')
-
-    expect(response.body[0].likes).toBe(0);
-})
-
-test('verify if title and post are missing', async () => {
-    const falsyPost = {
-        author: 'Who knows',
-        likes: 140
-    }
-
-    const resp = await api
-        .post('/api/blogs')
-        .send(falsyPost)
-        .expect(400)
-
-    console.log(resp)
+        const authors = blogsAtEnd.map(b => b.author)
+        expect(authors).not.toContain(toDel.author)
+    })
 })
 
 afterAll(() => {
