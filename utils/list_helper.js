a0648c1b4cc6b4e59a57e939b839b176c6aa5327
bloweyelashwish <yelizaveta.barkar@gmail.com>
2022-09-15 16:00:36 +0300

    Test total likes

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (list) => {
+    const likes = list.map(item => item.likes);
+
+    if (likes.length === 0) {
+        return 0;
+    }
+
+    const total = likes.reduce((acc, curr) => acc + curr, 0);
+    return total;
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
