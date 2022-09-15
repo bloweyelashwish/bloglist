cf7539e1bb7e67d60afa96be8916bf19c7080127
bloweyelashwish <yelizaveta.barkar@gmail.com>
2022-09-15 15:35:08 +0300

    Intro to testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,35 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es2021: true,
-    node: true,
+  "env": {
+    "node": true,
+    "commonjs": true,
+    "es2021": true,
+    "jest/globals": true
   },
-  extends: 'eslint:recommended',
-  parserOptions: {
-    ecmaVersion: 'latest',
+  "extends": "eslint:recommended",
+  "overrides": [
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest"
   },
-  rules: {
-    indent: ['error', 2],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'always'],
-    eqeqeq: 'error',
-    'no-trailing-spaces': 'error',
-    'object-curly-spacing': ['error', 'always'],
-    'arrow-spacing': ['error', { before: true, after: true }],
-    'no-console': 0,
-  },
-};
+  "plugins": [
+    "jest"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ]
+  }
+}
