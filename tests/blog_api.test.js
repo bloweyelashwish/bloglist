325a0ad7732e4ff2e57d8470e3e3d93217f1c840
bloweyelashwish <yelizaveta.barkar@gmail.com>
2022-09-15 19:51:38 +0300

    Ñdb for test initialized

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,7 +1,39 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-const {mongo} = require("mongoose");
+const Blog = require('../models/blogs')
+
+const initialBlogs = [
+    {
+        title: "Some blog title",
+        author: "Aaron",
+        url: "facebook.com/",
+        likes: 60
+    },
+    {
+        title: "Some blog title",
+        author: "Jake",
+        url: "facebook.com/",
+        likes: 2
+    },
+    {
+        title: "Some blog title",
+        author: "Melissa",
+        url: "facebook.com/",
+        likes: 30
+    }
+]
+
+//initialize the database before every test
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObj = new Blog(initialBlogs[0])
+    await blogObj.save()
+    blogObj = new Blog(initialBlogs[1])
+    await blogObj.save()
+    blogObj = new Blog(initialBlogs[2])
+    await blogObj.save()
+})
 
 const api = supertest(app)
 
@@ -22,6 +54,19 @@ test('the author of the first blog is Mark', async () => {
     const response = await api.get('/api/blogs')
 
     expect(response.body[0].author).toBe('Mark')
+})
+
+test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+test('a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs')
+    const authors = response.body.map(b => b.author);
+
+    expect(authors).toContain('Aaron')
 })
 
 afterAll(() => {
