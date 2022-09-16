59276bdb5e2ed7d9e22a386dd8009f8fb1883300
bloweyelashwish <yelizaveta.barkar@gmail.com>
2022-09-16 13:22:06 +0300

    Adding users/testing users

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('express-async-errors')
 const config = require("./utils/config")
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const app = express()
@@ -12,5 +13,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use("/api/blogs", blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
